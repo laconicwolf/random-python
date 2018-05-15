f27c3d2d3f8e49cc1df90394de3b0a3823dad6a8
Jake Miller <laconicwolf@gmail.com>
2018-05-15 13:29:03 -0500

    Update plot_bytes.py

--- plot_bytes.py
+++ plot_bytes.py
@@ -11,6 +11,7 @@ import base64
 import urllib.parse
 import argparse
 import os
+import time
 
 
 def decode_url_encoding(input_string):
@@ -24,9 +25,54 @@ def decode_url_encoding(input_string):
 
 
 def get_integer_list(input_bytes):
-    """Returns a list of byte integers.
+    """Returns a list of byte integers from a byte-string.
     """
     return [byte for byte in input_bytes]
+
+
+def create_byte_histogram(input_bytes):
+    """Returns a dictionary containing each byte value as a key and the
+    number of times it occures in the input bytes as a value.
+    """
+    bytes_dict = dict()
+    for byte in input_bytes:
+        bytes_dict[byte] = bytes_dict.get(byte, 0) + 1
+    return bytes_dict
+
+    
+def check_byte_representation(bytes_dict):
+    """Generates a list of all possible 256 bytes values and returns any
+    byte value that does not appear as a key in the supplied dictionary.
+    """
+    missing_byte_list = []
+    total_bytes = [byte for byte in range(256)]
+    for byte in total_bytes:
+        if byte not in bytes_dict:
+            missing_byte_list.append(byte)
+    return missing_byte_list
+        
+
+def plot_histogram(data):
+    """Plots a histogram from the given data.
+    """
+    plt.hist(data, color='g')
+    plt.title('Byte Histogram')
+    plt.ylabel('Occurence')
+    plt.xlabel('Byte Values')
+    plt.xticks(range(0, 256, 10))
+    plt.show()
+
+
+def plot_scatter(data):
+    """Plots a scatter chart from the given data.
+    """
+    positions = [i for i in range(len(data))]
+    plt.scatter(data_list, positions, edgecolors='r')
+    plt.title('Byte Scatter Plot')
+    plt.ylabel('Occurence')
+    plt.xlabel('Byte Values')
+    plt.xticks(range(0, 256, 10))
+    plt.show()
 
 
 def main():
@@ -40,21 +86,17 @@ def main():
     data_list = get_integer_list(data)
     integer_list = [i for i in range(256)]
     if args.plot_histogram:
-        plt.hist(data_list, color='g')
-        plt.title('Byte Histogram')
-        plt.ylabel('Occurence')
-        plt.xlabel('Byte Values')
-        plt.xticks(range(0, 256, 10))
-        plt.show()
+        plot_histogram(data_list)
     if args.plot_scatter:
-        positions = [i for i in range(len(data_list))]
-        plt.scatter(data_list, positions, edgecolors='r')
-        plt.title('Byte Scatter Plot')
-        plt.ylabel('Occurence')
-        plt.xlabel('Byte Values')
-        plt.xticks(range(0, 256, 10))
-        plt.show()
-    print(data[:50])
+        plot_scatter(data_list)
+    print("[*] Processing {}...".format(data[:50]))
+    time.sleep(1)
+    data_dict = create_byte_histogram(data)
+    print("[*] Checking if all bytes are represented")
+    time.sleep(1)
+    missing_bytes = check_byte_representation(data_dict)
+    missing_byte_count = sum([1 for byte in missing_bytes])
+    print("[+] {} bytes positions are not represented in the data".format(missing_byte_count))
 
 
 if __name__ == '__main__':
@@ -95,5 +137,6 @@ if __name__ == '__main__':
             print("\n[-] The file cannot be found or you do not have permission to open the file. Please check the path and try again\n")
             exit()
         else:
-            input_data = open(args.file, 'rb').read()
+            with open(args.file, 'rb') as fh:
+                input_data = fh.read()
     main()
