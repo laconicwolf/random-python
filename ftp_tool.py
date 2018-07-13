5ca63119bc98c2f340ef5b5806ca3b541af95d2e
Jake Miller <laconicwolf@gmail.com>
2018-07-13 14:45:00 -0500

    Update ftp_tool.py

--- ftp_tool.py
+++ ftp_tool.py
@@ -7,13 +7,16 @@ __version__ = '0.02'
 __description__ = 'A program to discover the FTP service, attempt anonymous login, and list directories.'
 
 
-import ipaddress
 import itertools
 import argparse
 import ftplib
 import os
 import csv
 import pprint
+import sys
+import re
+if sys.version.startswith('3'):
+    import ipaddress
 
 
 def ftp_anon_login(ftp_obj):
@@ -38,7 +41,8 @@ def parse_to_csv(data):
         try:
             csv_file = open(csv_name, 'a', newline='')
         except PermissionError:
-            print("\n[-] Permission denied to open the file {}. Check if the file is open and try again.\n".format(csv_name))
+            print("\n[-] Permission denied to open the file {}. Check if the file is open and try again.\n".format(
+                csv_name))
             exit()
         csv_writer = csv.writer(csv_file)
         print('\n[+]  {} exists. Appending to file!\n'.format(csv_name))
@@ -53,7 +57,7 @@ def list_directories(ftp_obj, depth=1):
     contains a contents of the subdirectory or None if it corresponds to a file. Adapted from:
     https://stackoverflow.com/questions/1854572/traversing-ftp-listing
     """
-    if depth >= max_depth:
+    if depth > max_depth:
         return ['depth > {}'.format(max_depth)]
     entries = {}
     for entry in (path for path in ftp_obj.nlst() if path not in ('.', '..')):
@@ -63,6 +67,9 @@ def list_directories(ftp_obj, depth=1):
             ftp_obj.cwd('..')
         except ftplib.error_perm:
             entries[entry] = None
+        except Exception as e:
+            print('An error occurred: {}'.format(e))
+            continue
     if entries is {}:
         ftp_obj.cwd('/')
         directories = ftp_obj.nlst()
@@ -72,8 +79,8 @@ def list_directories(ftp_obj, depth=1):
 
 
 def ip_range(input_string):
-    """Accepts a '-' specified ip range and returns a list
-    of ip addresses. Adapted to Python3 from:
+    """Accepts a dash specified range and returns a list of ip addresses
+    within that range. Adapted from:
     https://stackoverflow.com/questions/20525330/python-generate-a-list-of-ip-addresses-from-user-input
     """
     octets = input_string.split('.')
@@ -83,8 +90,22 @@ def ip_range(input_string):
     return addrs
 
 
+def cidr_ip_range(input_string):
+    """Accepts a CIDR range and returns a list of ip addresses
+    within the CIDR range.
+    """
+    addr_obj = ipaddress.ip_network(input_string)
+    addrs = [addr for addr in addr_obj.hosts()]
+    return addrs
+
+
 def main():
+    """Attempts FTP connections, anonymous logins, and list directories."""
+
+    # Initialize variable to store all the data for each address.
     data = []
+
+    # Attempts an FTP connection to each address.
     for addr in addrs:
         host_data = []
         ftp = ftplib.FTP()
@@ -93,19 +114,24 @@ def main():
             print('[+] {0}:{1} - Connection established'.format(addr, port))
             if args.verbose:
                 print('      {}'.format(banner))
-        except ftplib.all_errors:
-            print('[-] {0}:{1} - Unable to connect'.format(addr, port))
+        except ftplib.all_errors as e:
+            if args.verbose:
+                print('[-] {0}:{1} - Unable to connect'.format(addr, port))
             continue
+
+        # Attempts anonymous login.
+        anonymous_login = False
         if args.anon_login:
             login_message = ftp_anon_login(ftp)
             if login_message != '':
                 print('[+] {0}:{1} - Anonyomous login established'.format(addr, port))
                 anonymous_login = True
             else:
-                print('[-] {0}:{1} - Unable to log in. Permission Error'.format(addr, port))
-                anonymous_login = False
-        else:
-            anonymous_login = False
+                if args.verbose:
+                    print('[-] {0}:{1} - Unable to log in. Permission Error'.format(addr, port))
+
+        # Performs directory listing.
+        all_dirs = ''
         if args.list_dir:
             all_dirs = ''
             if anonymous_login is True:
@@ -113,8 +139,6 @@ def main():
                 print('[+] {0}:{1} - Directory Listing:'.format(addr, port))
                 all_dirs = pprint.pformat(dirs)
                 print(all_dirs)
-        else:
-            all_dirs = ''
         host_data.extend((addr, port, banner, anonymous_login, all_dirs))
         data.append(host_data)
     if args.csv:
@@ -144,8 +168,7 @@ if __name__ == '__main__':
                         type=int,
                         nargs='?',
                         const="1",
-                        help="Lists directories (one deep) if login is allowed. Specify a number to list further directories."
-                        )
+                        help="Lists directories (one deep) if login is allowed. Specify a number to list further directories.")
     parser.add_argument("-csv", "--csv",
                         nargs='?',
                         const='ftp_results.csv',
@@ -159,25 +182,75 @@ if __name__ == '__main__':
     if args.input_filename:
         if not os.path.exists(args.input_filename):
             parser.print_help()
-            print(
-                "\n[-] The file cannot be found or you do not have permission to open the file. Please check the path and try again\n")
+            print("\n[-] The file cannot be found or you do not have permission to open the file. Please check the path and try again\n")
             exit()
         addrs = open(args.input_filename).read().splitlines()
     if args.range:
+        if not '-' in args.range and not '/' in args.range:
+            if sys.version.startswith('3'):
+                parser.print_help()
+                print("\n[-] Please either specify a CIDR range or an octet range with a dash ('-').\n")
+                exit()
+            else:
+                parser.print_help()
+                print("\n[-] Please specify an octet range with a dash ('-').\n")
+                exit()
+
+        # https://www.regextester.com/93987
+        cidr_regex = r'^([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))?$'
+
+        # adapted from https://stackoverflow.com/questions/10086572/ip-address-validation-in-python-using-regex
+        dash_regex = r'^[\d+-?]{1,7}\.[\d+-?]{1,7}\.[\d+-?]{1,7}\.[\d+-?]{1,7}$'
+
         if '-' in args.range:
             if '/' in args.range:
-                print("\n[-] Unable to accept a CIDR and '-' specified range.\n")
-                exit()
-            addrs = ip_range(args.range)
-        else:
-            try:
-                addr_obj = ipaddress.ip_network(args.range)
-            except ValueError:
+                if sys.version.startswith('3'):
+                    parser.print_help()
+                    print("\n[-] Please either use CIDR notation or specify octet range with a dash ('-'), not both.\n")
+                    exit()
+                else:
+                    parser.print_help()
+                    print("\n[-] CIDR notation not supported with Python2. For CIDR notation, please use Python3.\n")
+                    exit()
+            if not re.findall(dash_regex, args.range):
                 parser.print_help()
-                print('\n[-] Please do not specify host bits. Example: -r 10.1.1.0/24\n')
+                print('\n[-] Invalid IP range detected. Please try again.\n')
                 exit()
-            addrs = [addr for addr in addr_obj.hosts()]
+            ip_addrs = ip_range(args.range)
+
+            # Additional validation to dump any octet larger than 255
+            addrs = []
+            for addr in ip_addrs:
+                octets = str(addr).split('.')
+                invalid_addr = [octet for octet in octets if int(octet) > 255]
+                if invalid_addr:
+                    continue
+                addrs.append(addr)
+        elif '/' in args.range:
+            if sys.version.startswith('2'):
+                parser.print_help()
+                print(
+                    "\n[-] CIDR notation not supported when runnng this script \
+                    with Python2. For CIDR notation, please use Python3.\n")
+                exit()
+            try:
+                if not re.findall(cidr_regex, args.range):
+                    parser.print_help()
+                    print('\n[-] Invalid CIDR range detected. Please try again.\n')
+                    exit()
+                addrs = cidr_ip_range(args.range)
+            except ValueError as error:
+                parser.print_help()
+                print('\n[-] Invalid CIDR range detected. Please try again.')
+                print('[-] {}\n'.format(error))
+                exit()
     if args.ipaddress:
+        if '/' in args.ipaddress:
+            print('\n[-] Invalid character ('/') detected in hostname/IP address.\n')
+            exit()
+        if '.' not in args.ipaddress:
+            print('\n[-] Invalid hostname/IP address detected.\n')
+            exit()
         addrs = [args.ipaddress]
     if args.csv:
         csv_name = args.csv
@@ -190,7 +263,7 @@ if __name__ == '__main__':
             print("\n[-] Permission denied to open the file {}. Check if the file is open and try again.".format(
                 csv_name))
             exit()
-    
+
     port = args.port
     max_depth = args.list_dir
 
