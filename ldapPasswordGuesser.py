018b0f2732be736ef6513e9b31d91664eb5afcbb
Jake Miller <laconicwolf@gmail.com>
2018-11-29 09:56:18 -0500

    Add files via upload

--- ldapPasswordGuesser.py
+++ ldapPasswordGuesser.py
@@ -19,7 +19,8 @@ import Queue
 # Required to import .Net assemblies. CLR is Common Language Runtime
 import clr
 
-# Adds the assemblies to CLR. CLR only includes a reference to System by default.
+# Adds the assemblies to CLR. 
+# CLR only includes a reference to System by default.
 clr.AddReference("System.DirectoryServices.AccountManagement")
 
 # Importing the specific namespaces
@@ -66,18 +67,21 @@ def manageQueue():
 def main():
     """Prints information about the guessing session and 
     starts the multithreading."""
-    word_banner = '{} version: {}. Coded by: {}'.format(sys.argv[0], __version__, __author__)
+    word_banner = '{} version: {}. Coded by: {}'.format(
+        sys.argv[0], __version__, __author__)
     print('=' * len(word_banner))
     print(word_banner)
     print('=' * len(word_banner))
     print
     for arg in vars(args):
         if getattr(args, arg):
-            print('{}: {}'.format(arg.title().replace('_',' '), getattr(args, arg)))
+            print('{}: {}'.format(
+                arg.title().replace('_',' '), getattr(args, arg)))
     print
-    print '[*] Guessing {} user(s) and {} password(s). {} total guesses.'.format(
+    print ('[*] Guessing {} user(s) and {} password(s). {} total'
+    ' guesses.'.format(
         len(usernames), len(passwords), len(usernames) * len(passwords)
-    )
+    ))
     print '[*] Only printing valid username/password combinations.\n'
 
     for i in range(args.threads):
@@ -90,7 +94,8 @@ def main():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Guess a supplied list of passwords against a supplied list of usernames.'
+        description=('Guess a supplied list of passwords against a supplied '
+        'list of usernames.')
     )
     parser.add_argument(
         '-u', '--username',
@@ -98,7 +103,8 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         '-d', '--domain',
-        help='Specify a domain. Defaults to the local domain of the computer the script is running from.'
+        help=('Specify a domain. Defaults to the local domain of the computer'
+        ' the script is running from.')
     )
     parser.add_argument(
         '-p', '--password',
@@ -106,11 +112,13 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         '-uf', '--username_file',
-        help='Specify the path of a file containing usernames to guess password(s) against.'
+        help=('Specify the path of a file containing usernames to guess '
+        'password(s) against.')
     )
     parser.add_argument(
         '-pf', '--password_file',
-        help='Specify the path of a file containing passwords to guess against username(s).'
+        help=('Specify the path of a file containing passwords to guess '
+        'against username(s).')
     )
     parser.add_argument(
         "-t", "--threads",
@@ -124,19 +132,23 @@ if __name__ == '__main__':
 
     if not args.username and not args.username_file:
         parser.print_help()
-        print '\n[-] Please specify either a single username (-u) or the path to a file listing usernames (-uf).'
+        print ('\n[-] Please specify either a single username (-u) or the '
+        'path to a file listing usernames (-uf).')
         exit()
     if not args.password and not args.password_file:
         parser.print_help()
-        print '\n[-] Please specify either a single password (-p) or the path to a file listing passwords (-pf).'
+        print ('\n[-] Please specify either a single password (-p) or the '
+        'path to a file listing passwords (-pf).')
         exit()
     if args.username and args.username_file:
         parser.print_help()
-        print '\n[-] Please specify either a single username (-u) or the path to a file listing usernames (-uf). Not both.'
+        print ('\n[-] Please specify either a single username (-u) or the '
+        'path to a file listing usernames (-uf). Not both.')
         exit()
     if args.password and args.password_file:
         parser.print_help()
-        print '\n[-] Please specify either a single password (-p) or the path to a file listing passwords (-pf). Not both.'
+        print ('\n[-] Please specify either a single password (-p) or the '
+        'path to a file listing passwords (-pf). Not both.')
         exit()
 
     usernames = passwords = []
@@ -145,7 +157,9 @@ if __name__ == '__main__':
         usernames = [args.username]
     if args.username_file:
         if not os.path.exists(args.username_file):
-            print '\n[-] The file {} does not exist or you do not have access to it. Please check the path and try again.'.format(args.username_file)
+            print ('\n[-] The file {} does not exist or you do not have '
+            'access to it. Please check the path and try again.'.format(
+                args.username_file))
             exit()
         with open(args.username_file) as fh:
             usernames = fh.read().splitlines()
@@ -153,7 +167,9 @@ if __name__ == '__main__':
         passwords = [args.password]
     if args.password_file:
         if not os.path.exists(args.password_file):
-            print '\n[-] The file {} does not exist or you do not have access to it. Please check the path and try again.'.format(args.password_file)
+            print ('\n[-] The file {} does not exist or you do not have '
+            'access to it. Please check the path and try again.'.format(
+                args.password_file))
             exit()
         with open(args.password_file) as fh:
             passwords = fh.read().splitlines()
